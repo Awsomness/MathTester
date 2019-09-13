a7c9b555a139493d3223a4344c59b33cc2144280
Nathanial <nathanial.kilpatrick.2022@student.lhschools.org>
2019-09-13 09:50:11 -0400

    Reset page

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,8 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("Hello Source Control");
-        System.out.println("And welcome to my java file");
 
 
 

6694c7d90be7eb290f740890d8c2fb61a9973802
Nathanial <nathanial.kilpatrick.2022@student.lhschools.org>
2019-09-13 10:01:19 -0400

    Added "first Word and second Word" to main method

---
+++ src/MathTester.java
@@ -0,0 +1,13 @@
+public class MathTester {
+          public static void main(String[] args){
+
+    String firstWord;
+    String secondWord;
+
+
+    firstWord = "dog";
+    secondWord = "house";
+
+        System.out.println(firstWord+secondWord);
+    }
+}
